6e32cb24406d60baca466ce675264e4b7e1f089f
Ryan Grenz <ryan.grenz@fil.com>
2017-05-18 17:03:41 +0100

    Remove debugging

--- assets/decrypt.sh
+++ assets/decrypt.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-set -x
-
 file="${1}"
 encryption_key="${2}"
-gpg --yes --no-use-agent --batch --passphrase="${encryption_key}" -o "${file%.gpg}" -d "${file}" 
+gpg --yes --no-use-agent --batch --passphrase="${encryption_key}" -o "${file%.gpg}" -d "${file}"
