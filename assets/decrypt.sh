943ee56879d65e88a3748fb49ee155078eb174a9
Jim Conner <james.conner@fil.com>
2017-05-19 11:57:25 +0100

    gpg version no longer needs --no-use-agent
    * unlike the version we have on our macs

--- assets/decrypt.sh
+++ assets/decrypt.sh
@@ -2,4 +2,4 @@
 
 file="${1}"
 encryption_key="${2}"
-gpg --yes --no-use-agent --batch --passphrase="${encryption_key}" -o "${file%.gpg}" -d "${file}"
+gpg --yes --batch --passphrase="${encryption_key}" -o "${file%.gpg}" -d "${file}"
