b23861d7260fb85cd4d26a61180bbae85a302b08
Stuart Willcock <stuart.willcock@fil.com>
2021-07-08 11:49:22 +0100

    Added metadata to improve concourse logging info
    Current s3 resource didn’t always show much information on files changed, bucket or endpoint related info, when resource was passed through jobs.
    
    This change enables the s3 encrypt resource to output resource metadata,
    including attributes like bucket, s3 endpoint, path to file in bucket,
    and filenames (with respective size and last modified date with
    timezone)
    
    Currently this metadata information is only viewable in the GET of a
    resource from a job on concourse, as the changes were only implemented in the assets/in script.
    
    Signed-off-by: Sushant Kapoor <sushant.kapoor@fil.com>

---
+++ assets/metadata.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import requests
+import sys
+import json
+import dpath.util
+import dateutil.parser as dp
+from datetime import datetime
+
+def extract_vars_from_payload(payload):
+    try:
+        bucket = payload['source']['bucket']
+        endpoint = payload['source']['endpoint']
+        path = payload['source']['path']
+        version_ref = payload['version']['ref']
+    except (KeyError, TypeError) as e:
+        print("Error processing payload from concourse")
+        print("Required source parameters are bucket, endpoint, path and version_ref")
+        print(e)
+        sys.exit(1)
+    return(bucket, endpoint, path, version_ref)
+
+
+if __name__ == "__main__":
+    try:
+        payload = sys.stdin.read()
+        file_info = ""
+        if len(sys.argv) > 1:
+            file_info=json.loads(sys.argv[1])
+
+        bucket, endpoint, path, version_ref = extract_vars_from_payload(json.loads(payload))
+
+        metadata = [{"name": "bucket", "value": bucket}, {"name": "endpoint", "value": endpoint}, {"name": "path", "value": path}]
+
+        if file_info != "":
+            for file in file_info:
+                new_time = dp.isoparse(file['last_changed'])
+                file['last_changed'] = new_time.strftime("%d/%m/%Y, %H:%M:%S %Z")
+                metadata.append({"name": f"{file['file_name']}", "value" : f"Size: {file['file_size']} bytes, Last-modified: {file['last_changed']}"})
+
+        print(json.dumps({ "version": { "ref": version_ref }, "metadata": metadata }))
+
+    except Exception as e:
+        print("Unexpected error encountered in `main`")
+        print(e)
+        sys.exit(1)
